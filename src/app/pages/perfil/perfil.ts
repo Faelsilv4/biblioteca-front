a0e3ed0932ec31c6152e9e0074f60fe4f2ec4cea
Faelsilv4 <rafasilvav7@gmail.com>
2026-05-29 20:13:16 -0300

    Implementa perfil e fluxo de emprestimos

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
-export class Perfil {}
+export class Perfil implements OnInit {
+
+  nome = '';
+  email = '';
+  tipoUsuario = '';
+
+  ngOnInit(): void {
+    this.nome = localStorage.getItem('nome') || '';
+    this.email = localStorage.getItem('email') || '';
+    this.tipoUsuario = localStorage.getItem('tipoUsuario') || '';
+  }
+}
